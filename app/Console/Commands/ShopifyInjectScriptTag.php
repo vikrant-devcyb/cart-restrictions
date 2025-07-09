a47ca43f00ad65849757509f78f1665b88ff89f4
Manpreet Singh <manpreet.cyberbells@gmail.com>
2025-07-09 17:34:01 +0530

    IChanges

--- app/Console/Commands/ShopifyInjectScriptTag.php
+++ app/Console/Commands/ShopifyInjectScriptTag.php
@@ -30,6 +30,7 @@ class ShopifyInjectScriptTag extends Command
             'X-Shopify-Access-Token' => $accessToken
         ])->get("https://{$shopDomain}/admin/api/2024-04/script_tags.json");
 
+        echo"<pre>"; print_r($scriptUrl);  echo"<br>";
         echo"<pre>"; print_r($accessToken);  echo"<br>";
         // echo"<pre>"; print_r($response);  die;
         if (!$response->successful()) {
