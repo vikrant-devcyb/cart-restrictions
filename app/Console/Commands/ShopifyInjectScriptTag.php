4bcb3edfad45927082ea4dd75fcd371d54b443be
Manpreet Singh <manpreet.cyberbells@gmail.com>
2025-07-09 17:44:03 +0530

    Remove unwanted stuff

--- app/Console/Commands/ShopifyInjectScriptTag.php
+++ app/Console/Commands/ShopifyInjectScriptTag.php
@@ -30,11 +30,6 @@ class ShopifyInjectScriptTag extends Command
             'X-Shopify-Access-Token' => $accessToken
         ])->get("https://{$shopDomain}/admin/api/2024-04/script_tags.json");
 
-        echo"<pre>"; print_r($shop);  echo"<br>";
-        echo"<pre>"; print_r($shopDomain);  echo"<br>";
-        echo"<pre>"; print_r($scriptUrl);  echo"<br>";
-        echo"<pre>"; print_r($accessToken);  echo"<br>";
-        // echo"<pre>"; print_r($response);  die;
         if (!$response->successful()) {
             $this->error("Failed to fetch ScriptTags: " . $response->body());
             return;
