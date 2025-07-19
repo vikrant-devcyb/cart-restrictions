cc2007c047f8148aff62c384def51f265bfcdf58
manpreetcyberbells <manpreet.cyberbells@gmail.com>
2025-07-19 12:12:15 +0530

    sqlite fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,16 @@
 # Copy .env if not already present
 [ -f .env ] || cp .env.example .env
 
+# Create SQLite file if it doesn't exist
+if [ "$DB_CONNECTION" = "sqlite" ]; then
+    DB_PATH=${DB_DATABASE:-"/app/database/database.sqlite"}
+    if [ ! -f "$DB_PATH" ]; then
+        echo "Creating SQLite file at $DB_PATH"
+        mkdir -p "$(dirname "$DB_PATH")"
+        touch "$DB_PATH"
+    fi
+fi
+
 # Generate app key if not present
 php artisan key:generate --force
 
