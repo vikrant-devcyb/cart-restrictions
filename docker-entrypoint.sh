987a0fff8c8c0680c14fa77498bac8fc0c97578a
manpreetcyberbells <manpreet.cyberbells@gmail.com>
2025-08-18 13:55:09 +0530

    port issue

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -28,4 +28,4 @@ php artisan queue:work --daemon &
 [ -f storage/app/private/shops.json ] && chmod 600 storage/app/private/shops.json
 
 # Start server
-php -S 0.0.0.0:8000 -t public
+php -S 0.0.0.0:8080 -t public
